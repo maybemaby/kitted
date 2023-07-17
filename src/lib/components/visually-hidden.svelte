4f4743e8821fc6eb447ea54e27547d115d76d0c2
maybemaby <74226064+maybemaby@users.noreply.github.com>
2023-07-16 20:28:09 -0700

    +visually hidden component for screenreaders

---
+++ src/lib/components/visually-hidden.svelte
@@ -0,0 +1,14 @@
+<span class="sr-only">
+	<slot />
+</span>
+
+<style>
+	.sr-only {
+		position: absolute;
+		left: -10000px;
+		top: auto;
+		width: 1px;
+		height: 1px;
+		overflow: hidden;
+	}
+</style>
